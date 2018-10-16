11bff72e7fc34334ae0509402069e2f2d8a9ad52
Cody Rickman <cody@creativemindstudios.com>
2018-10-16 16:31:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz (int)
     "Fizz"
   elsif int % 5 == 0 && int % 3 != 0
     "Buzz"
-  else=if int % 5 == 0 && int % 3 == 0
+  elsif int % 5 == 0 && int % 3 == 0
     "FizzBuzz"
   else int % 5 != 0 && int % 3 != 0
     nil
