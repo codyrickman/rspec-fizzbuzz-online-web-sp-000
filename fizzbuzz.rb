38bf8a05ee3366ac1f28528fd3e22b4bac42345b
Cody Rickman <cody@creativemindstudios.com>
2018-10-16 16:25:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz (val)
-  if int % 3 == 0
+def fizzbuzz (int)
+  if int % 3 == 0 && int % 5 != 0
     "Fizz"
-  elsif int % 3 != 0
+  elsif int % 5 == 0 && int % 3 != 0
+    "Buzz"
+  else=if int % 5 == 0 && int % 3 == 0
+    "FizzBuzz"
+  elsif int % 5 != 0 && int % 3 != 0
     nil
   end
 end
